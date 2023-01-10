fd748de4e0fe472ad1d4f29046367dad32c59f9e
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-10 23:46:02 +0000

    task png is created

---
+++ task-treacker/src/assets/TaskPng.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+const TaskPng = (props) => (
+    <svg 
+        xmlns="http://www.w3.org/2000/svg"
+        width={100}
+        height={100}
+        viewBox="0 0 512 512"
+
+        {...props}
+    >
+        <path d="M116.3 1c-3.4 1.4-4.5 4.5-6.2 18.4-1.1 9.2-2"></>;
+        <path d="M363 199.5V231h9v-6.3c0-6 .2-6.5 3.8-9.913.7-3.7"></>;
+    </svg>
+
+);
+
+export default TaskPng;
