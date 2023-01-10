8b368ce18ade1ea318182f4a8d356b5cadade285
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-10 22:40:12 +0000

    header and app.js is re-set

---
+++ task-treacker/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import TaskPng from "../../assets/TaskPng";
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Header</h1>
+        </div>
+    )
+}
+
+export default Header;
