7abd9fb3ab070d91f47692e8d4fca3a77ed97ea2
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-11 22:07:03 +0000

    header.jsx is updated

--- task-treacker/src/components/header/Header.jsx
+++ task-treacker/src/components/header/Header.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-
 import TaskPng from "../../assets/TaskPng";
 
 
 const Header = () => {
     return (
-        <div>
-            <div className="d-flex justify-content-evenly align-items-center"></div>
-            <TaskPng/>
-            <h1>TASK TRACKER</h1>
+        <div className="d-flex justify-content-evenly align-items-center">
+            <TaskPng />
+            <h1 className="display-5">TASK TRACKER</h1>
         </div>
-    )
-}
+    );
+};
 
 export default Header;
